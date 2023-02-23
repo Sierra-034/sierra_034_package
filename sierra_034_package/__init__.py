2d9c98588e82e48d9c87e7baf4638137ac612667
samgoz <samuel.gomezv@elektra.com.mx>
2023-02-23 12:20:32 -0600

    paquete listo

--- sierra_034_package/__init__.py
+++ sierra_034_package/__init__.py
@@ -1,1 +1,3 @@
+from sierra_034_package.workshops import unreleased
+
 print('sierra_034_package saludos!!')
--- sierra_034_package/__main__.py
+++ sierra_034_package/__main__.py
@@ -1,5 +1,7 @@
 from sierra_034_package import unreleased
 
 if __name__ == '__main__':
+    print('>> Comienza ejecución de paquete')
     workshops = unreleased()
     print(workshops)
+    print('>> Termina ejecución de paquete')
